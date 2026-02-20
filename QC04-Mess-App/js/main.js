cb411c850932abb92002fdae4f75daed1abb2f03
Sagar Sah <thedailyclips365@gmail.com>
2026-02-20 23:50:01 +0530

    Update main.js

--- QC04-Mess-App/js/main.js
+++ QC04-Mess-App/js/main.js
@@ -8,3 +8,12 @@ document.addEventListener("DOMContentLoaded", () => {
     if (splash) splash.classList.add("hide");
   }, 1200);
 });
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./sw.js")
+      .then(() => console.log("Service Worker Registered"))
+      .catch(err => console.log("SW Error:", err));
+  });
+}
