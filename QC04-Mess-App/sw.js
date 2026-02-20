41ea78bfdade45e9093e24942fbb2b4b1a1d6cf7
Sagar Sah <thedailyclips365@gmail.com>
2026-02-20 23:47:58 +0530

    Add files via upload

---
+++ QC04-Mess-App/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "qc04-mess-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/data.js",
+  "./js/ui.js",
+  "./js/utils.js",
+  "./js/main.js",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
